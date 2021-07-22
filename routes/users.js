49639ab266a613140bd423cfad126c7b54229181
Chankapaanaa <68669502+Chankapaanaa@users.noreply.github.com>
2021-07-22 15:15:34 -0400

    Delete routes directory

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const user = require("./handler/user");
-
-/* GET users listing. */
-router.post("/login", user.login);
-
-module.exports = router;
